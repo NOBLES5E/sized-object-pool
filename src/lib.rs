41dd91e15f1211624b986c75eac8f76934288ab8
Xiangru Lian <admin@mail.xrlian.com>
2021-03-16 22:54:52 +0800

    [feat] use dynamic pool to return owned items

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,5 @@
-use object_pool::Pool;
-pub use object_pool::Reusable;
+use dynamic_pool::DynamicPool;
+pub use dynamic_pool::{DynamicPoolItem, DynamicReset};
 use thiserror::Error;
 
 #[derive(Error, Debug)]
@@ -13,18 +13,19 @@ pub trait SizedAllocatable {
     fn size(&self) -> usize;
 }
 
-pub struct SizedPool<T: SizedAllocatable> {
+pub struct SizedPool<T: SizedAllocatable + DynamicReset> {
     /// each entry represents an allocation queue of 2**n bytes block
-    sub_pools: Vec<Pool<T>>,
+    sub_pools: Vec<DynamicPool<T>>,
 }
 
-impl<T: SizedAllocatable> SizedPool<T> {
+impl<T: SizedAllocatable + DynamicReset> SizedPool<T> {
     /// cap is the capacity of each subpool, pool_size_power_of_two is the number of subpools,
     /// init_fn takes the pool_size (the power of two) as input and outputs the reusable resource
-    pub fn new(cap: usize, pool_size_power_of_two: u32) -> Self {
+    pub fn new(cap: usize, pool_size_power_of_two: u32, max_pool_size: usize) -> Self {
         let mut pools = Vec::new();
         for pool_power in 0..pool_size_power_of_two {
-            let pool = Pool::new(cap, || T::new(2_usize.pow(pool_power)));
+            let pool =
+                DynamicPool::new(cap, max_pool_size, move || T::new(2_usize.pow(pool_power)));
             pools.push(pool);
         }
         Self { sub_pools: pools }
@@ -34,29 +35,13 @@ impl<T: SizedAllocatable> SizedPool<T> {
         (size.next_power_of_two().trailing_zeros()) as usize
     }
 
-    fn get_subpool(&self, size: usize) -> Result<&Pool<T>, SizedPoolError> {
+    fn get_subpool(&self, size: usize) -> Result<&DynamicPool<T>, SizedPoolError> {
         self.sub_pools
             .get(self.get_subpool_location(size))
             .ok_or(SizedPoolError::SizeExceedMaxSize)
     }
 
-    pub fn try_pull(&self, size: usize) -> Result<Option<Reusable<T>>, SizedPoolError> {
-        Ok(self.get_subpool(size)?.try_pull())
-    }
-
-    pub fn pull(&self, size: usize) -> Result<Reusable<T>, SizedPoolError> {
-        let reusable = self.try_pull(size)?;
-        match reusable {
-            None => Ok(Reusable::new(
-                self.get_subpool(size)?,
-                T::new(2_usize.pow(self.get_subpool_location(size) as u32)),
-            )),
-            Some(x) => Ok(x),
-        }
-    }
-
-    pub fn attach(&self, t: T) -> Result<(), SizedPoolError> {
-        self.get_subpool(t.size())?.attach(t);
-        Ok(())
+    pub fn try_pull(&self, size: usize) -> Result<Option<DynamicPoolItem<T>>, SizedPoolError> {
+        Ok(self.get_subpool(size)?.try_take())
     }
 }
